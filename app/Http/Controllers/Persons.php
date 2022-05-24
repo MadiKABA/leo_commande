0f7c3f061a3974bce0f71affe71194fe7cbe36f3
MadiKABA <madikaba2004@gmail.com>
2022-05-24 18:11:13 +0000

    ajout de la lchaine de connexion php simple

--- app/Http/Controllers/Persons.php
+++ app/Http/Controllers/Persons.php
@@ -15,4 +15,24 @@ class Persons extends Controller
        
        
     }
+    /*code de connexion avec php simple
+    <?php
+$dsn="driver={HFSQL};Server Name=localhost;Serve Port=4900;Database=dbtest;UID=admin; PWD=";
+
+
+$conn = odbc_connect($dsn, '', '');
+
+
+if($conn===false) {
+    echo "erreur cnx odbc : " . odbc_errormsg()."\r\n";
+    } else {
+        echo "success cnx odbc : " . odbc_errormsg()."\r\n";
+        $req = "select * from categories;";
+        $rest=odbc_do($conn,$req);
+
+        while (odbc_fetch_into($rest, $rowt)){
+        echo $rowt[0]." ".$rowt[1]."<br>";
+        }
+    }*/
+
 }
